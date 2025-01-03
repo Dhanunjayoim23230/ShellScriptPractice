8241fb5f8e4ac50bbf3cd05d3517cc30c590be4d
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-03 06:10:47 +0530

    Added new input variabls file

---
+++ 02-Variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Person1:Vrushali
+Person2:Nanna
+#No space before and after equal operator
+
+
+echo "$Person1:: good morning $Person2"
+echo "$Person2:: good morning $Person1"
+echo "$Person1:: what are you doing $Person2?"
+echo "$Person2:: i am practicing shell script $Person1 and what are you doing $Person1"
+echo "$Person1:: I am playing $Person2, playing with numbers and alphabets and shapes"
+echo "$Person2:: good! keep playing $Person1"
