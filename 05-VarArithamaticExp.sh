92e4c12bb562988ab208b17f73529dd1cf13c3f3
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-03 22:08:04 +0530

    Arithamtic expression by using variables

---
+++ 05-VarArithamaticExp.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+A=$1
+B=$2
+
+#Arithamtic expressions by USING Variables  command $(())
+
+echo "A+B=$((A+B))"
+
+echo "A-B =$((A-B))"
+
+echo "A*B=$((A*B))"
+
+echo "A/B=$((A/B))"
