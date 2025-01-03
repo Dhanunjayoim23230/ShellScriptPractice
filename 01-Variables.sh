799bb4f2695a8abc75fca430c858693f74a4342a
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-03 05:57:36 +0530

    New file variables file added

---
+++ 01-Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Vrushali:: good morning Nanna"
+echo "Nanna:: good morning Vrushali"
+echo "Vrshali:: what are you doing Nanna?"
+echo "Nanna:: i am practicing shell script Vrushali and what are you doing Vrushali"
+echo "Vrushali:: I am playing Nanna, playing with numbers and alphabets and shapes"
+echo "Nanna:: good! keep playing Vrushali"
