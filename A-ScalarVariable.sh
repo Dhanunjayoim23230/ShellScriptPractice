20ed8963cde96f41698ba1a2215bf1d1a1e2a605
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-04 22:03:42 +0530

    scalar script updated

--- A-ScalarVariable.sh
+++ A-ScalarVariable.sh
@@ -6,12 +6,25 @@
 A=10
 B=20
 
-Val= {'expr $A + $B '}
+Val=expr $A + $B
 
 echo "Addition: $Val"
 
-#Sum='expr $Var_1 + $Var_2'
-#Addition='expr 2 + 2'
+Sum="(($A + $B))
+Addition='expr 2 + 2'
 echo "Sum of two: $Sum"
 echo "Additin of : $Addition"
 echo "Successfully created scalar variable-> used scalar variable syntax"
+
+echo "************************************************"
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "Process id of last command in background: $!"
+
+echo "************************************************"
