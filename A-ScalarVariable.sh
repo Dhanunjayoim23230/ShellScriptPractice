7d9deccf2de2045f5c291169eb695aae25518f1a
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-04 20:29:51 +0530

    scalar script updated

--- A-ScalarVariable.sh
+++ A-ScalarVariable.sh
@@ -3,10 +3,10 @@
 #Write script using scalar variable
 # syntax Varible_name=Variable_value
 
-Var_1= 10
-Var_2= 20
+Var_1=10
+Var_2=20
 
-Sum= $Var_1 + $Var_2
+Sum= "{$Var_1 + $Var_2}"
 echo "Sum of two: $Sum"
 
 echo "Successfully created scalar variable-> used scalar variable syntax"
