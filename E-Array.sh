6cace63382373ebdcfaf5ff09abd841269b7c3c0
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-05 21:25:24 +0530

    New script added array usage

--- E-Array.sh
+++ E-Array.sh
@@ -11,11 +11,16 @@ Arr[1]=2
 Arr[2]=3
 Arr[A]={4,5,6,7,8}
 
+# To print a selected index element
+selected_index=3
+echo “Selected index element at index $selected_index:”
+echo “${arr[$selected_index]}”
+
 sum=` &(({Arr[0]} + {Arr[1]}))  `
 echo "print output of :$sum"
-diff=` expr ${Arr[2]} - ${Arr[A0]} `
+diff=` expr ${Arr[2]} - ${Arr[$A0]} `
 echo "print output of :$diff"
-multiply= `expr ${Arr[A1]} * ${Arr[A2]} `
+multiply= `expr ${Arr[$A1]} * ${Arr[$A2]} `
 echo "print output of :$multiply"
-divide= `expr $A{rr[A3]} / ${Arr[A4]} `
+divide= `expr $A{rr[A$3]} / ${Arr[$A4]} `
 echo "print output of :$divide"
