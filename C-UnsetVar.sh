9b3d528bb7f745f240011141da45a0b35864b572
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-05 05:47:09 +0530

    scalar script updated

---
+++ C-UnsetVar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Unsetting Variables-Unsetting or deleting a variable directs the shell to remove the variable 
+#from the list of variables that it tracks. Once you unset a variable, you cannot access the stored 
+#value in the variable.Ex:unset variable_name
+
+
+VAR1= 20
+VAR2= 10
+
+SUM=` expr VAR1 = VAR2 `
+echo "print sum of: $SUM"
+unset VAR2
+
+VAR2=$1
+
+SUM2=` expr VAR1 = VAR2 `
+echo "print sum after unset VAR2 and reassign value: $SUM2"
