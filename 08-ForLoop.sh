e02fa21030a290f57d8780f0dfcb08cdd6feff02
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-06 22:46:10 +0530

    New script added for loop table creation

---
+++ 08-ForLoop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Sum of all the numbers between 1 to 100 including both that are divisible by 7
+
+#1.Enter starting number.
+#2.Enter ending number.
+#3.Check divisilbe number with modulus zero
+#4.Print the divisior in output.
+#5.Repeat the same till reaching end number.
+
+echo "enter starting number $1"
+echo "Enter ending number $2"
+echo "Enter divisible number $3"
+
+
+A=$1
+B=$2
+C=$3
+
+output= ` expr $B % $A`
+echo "print output value which is exact divisible by $3"
