4d5b77760901f0ef4df038a2087dc4f272848e2a
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-07 05:28:58 +0530

    New script added for loop table creation

--- 08-ForLoop.sh
+++ 08-ForLoop.sh
@@ -20,11 +20,11 @@ C=$3
 for A in ` seq 1 $B `
 do
 output=$((` expr $A % $C `)) 
-if [$output -eq 0]
-then
-echo "print output value which is exact divisible by $3 : $output"
-echo "print divisor value $A"
-else 
-echo "MOdulus for $A-->$output"
-fi
-done
+# if [$output -eq 0]
+# then
+# echo "print output value which is exact divisible by $3 : $output"
+# echo "print divisor value $A"
+# else 
+ echo "MOdulus for $A-->$output"
+# fi
+ done
