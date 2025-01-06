0198d49aded653db02c2cddc26615a8189d7749f
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-06 21:52:57 +0530

    New script added array usage

--- 06-forloop.sh
+++ 06-forloop.sh
@@ -2,3 +2,33 @@
 
 #Creating shell script using for loop
 
+#Uploading file in server path
+#checking file exist or not
+#Login  with user id idamprod
+# go to path
+# connect sftp user
+# got to the path
+# run mput file-name.csv
+# chagne permissions to 0775
+#exit from sftp server
+#exit from server
+
+#Creating table using for loop
+#1.Enter the i/p table number
+#2.MUltiply i/p * 1 equal to o/p
+#3.Print Multipication and o/p in proper table format.
+#4.Increment multiplied value as 1+1
+#5.Again Multiply table number with increment value get the o/p
+#6.Print again Multiplication and o/p in proper table format.
+#7.Repeat the same till increment value reach to 10.
+
+#Required variable creation
+
+echo "enter input table number"
+Table=$1
+echo "Enter increment value"
+A=1
+
+Multply=$(( `expr $Table + $A `))
+echo " $Table * $A = $Multiply"
+
