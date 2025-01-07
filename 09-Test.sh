e4c49dc3744ae1266c2b771d9bb594a0ea83379f
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2025-01-07 06:17:03 +0530

    New script added for loop table creation

--- 09-Test.sh
+++ 09-Test.sh
@@ -6,8 +6,11 @@ sum=0
 while [ $num -gt 0 ]
 do
     mod=$((num % 10))    #It will split each digits
+    echo "mod:$mod"
     sum=$((sum + mod))   #Add each digit to sum
+    echo "sum:$sum"
     num=$((num / 10))    #divide num by 10.
+    echo "num:$num"
 done
 
 echo $sum
