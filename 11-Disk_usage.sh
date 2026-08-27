159fa430b99e122adf65cd955ad340cf05dff00b
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-08-27 08:21:26 +0530

    practice disk usage

--- 11-Disk_usage.sh
+++ 11-Disk_usage.sh
@@ -13,7 +13,7 @@ do
     echo "$partition"
     if [ "$usage" -gt 10 ]
     then
-        msg+=$(echo "High disc usage: "$usage" and partition is : "$partition")
+        msg+=$(echo "High disc usage: "$usage" and partition is : "$partition"")
     fi
 
 done <<< $DISC_USAGE
