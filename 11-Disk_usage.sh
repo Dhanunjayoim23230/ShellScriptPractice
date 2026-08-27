93b5bf2983d9d7fd72516d883056cb2de2afab32
dhanunjayoim23230 <dhanunjayoim23230@gmail.com>
2026-08-27 08:19:45 +0530

    practice disk usage

--- 11-Disk_usage.sh
+++ 11-Disk_usage.sh
@@ -9,5 +9,11 @@ while read -r line
 do
     usage=$(echo "$line" | awk -F " " '{print $6F}' | cut -d "%" -f1)
     echo "$usage"
+    partition=$(echo "$line" | awk -F " " '{print $3F}')
+    echo "$partition"
+    if [ "$usage" -gt 10 ]
+    then
+        msg+=$(echo "High disc usage: "$usage" and partition is : "$partition")"
+    fi
 
 done <<< $DISC_USAGE
